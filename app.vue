54bfde8c2ad3f9db12f6425fda0570853d8041ca
vircoding <nluismi80@gmail.com>
2024-10-08 14:53:36 -0400

    default layout

--- app.vue
+++ app.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="font-quicksand text-azure-950 md:text-lg dark:text-gray-200">
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
---
+++ tailwind.config.ts
@@ -0,0 +1,28 @@
+import type { Config } from 'tailwindcss';
+
+export default <Partial<Config>>{
+  theme: {
+    extend: {
+      fontFamily: {
+        quicksand: ['Quicksand', 'sans-serif'], // 300, 400, 500, 600, 700
+      },
+      colors: {
+        lbackground: '#ecf7f7',
+        dbackground: '#161212',
+        azure: {
+          50: '#ecf6fe',
+          100: '#d6ecfc',
+          200: '#b5dffa',
+          300: '#82cbf6',
+          400: '#46adf2',
+          500: '#1183ea',
+          600: '#1668cc',
+          700: '#1c59b4',
+          800: '#1c478f',
+          900: '#1d3e6f',
+          950: '#162743',
+        },
+      },
+    },
+  },
+};
